c9b9cce7d7ea797b1028f0fee2c23b49c1a2f4f6
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-12-07 22:27:21 +0200

    postgres dependens

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  password: "root",
-  host: "localhost",
-  port: "5432",
-  database: "snake-game-db",
+  password: "5QUff4psWi8YyZbgxzEf",
+  host: "containers-us-west-148.railway.app",
+  port: "5800",
+  database: "railway",
 });
 
 module.exports = pool;
