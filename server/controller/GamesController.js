f059dd937cad6adc92913383f864e7a43fd2a048
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-12-08 18:28:42 +0200

    added try catch

--- server/controller/GamesController.js
+++ server/controller/GamesController.js
@@ -1,22 +1,40 @@
 const db = require("../db");
 class GamesController {
   async createGame(req, res) {
-    const { name, score } = req.body;
-    const newGame = await db.query(
-      "INSERT INTO games (name, score) VALUES ($1, $2) RETURNING *",
-      [name, +score]
-    );
-    res.json(newGame.rows[0]);
+    try {
+      const { name, score } = req.body;
+      const newGame = await db.query(
+        "INSERT INTO games (name, score) VALUES ($1, $2) RETURNING *",
+        [name, +score]
+      );
+      res.json(newGame.rows[0]);
+    } catch (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    }
   }
   async getGames(req, res) {
-    const games = await db.query("SELECT * FROM games");
-    res.json(games.rows);
+    try {
+      const games = await db.query("SELECT * FROM games");
+      res.json(games.rows);
+    } catch (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    }
   }
   async getTopGames(req, res) {
-    const topGames = await db.query(
-      "SELECT * FROM games ORDER BY score DESC LIMIT 10;"
-    );
-    res.json(topGames.rows);
+    try {
+      const topGames = await db.query(
+        "SELECT * FROM games ORDER BY score DESC LIMIT 10;"
+      );
+      res.json(topGames.rows);
+    } catch (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    }
   }
 }
 module.exports = new GamesController();
